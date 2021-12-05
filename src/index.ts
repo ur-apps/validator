3ccf6a5549286cb0a175b5541ba11867e3527470
Garik <garikfreedomyan@gmail.com>
2021-12-05 18:05:42 +0300

    fix and add imports

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
+export * from './constants';
+export { array, boolean, number, object, string } from './schemas';
 export * from './types';
 export * from './validator';
-export * from './regexps';
 export { Validator as default } from './validator';
