273b4808fdd10f49e51cf1d87de2f73a12fed95c
Garik <garikfreedomyan@gmail.com>
2021-12-05 18:00:21 +0300

    replaced messages and regexps in constants folder

--- src/constants/messages.ts
+++ src/constants/messages.ts
@@ -1,11 +1,14 @@
-import type { TValidationMessages } from './types';
+import type { TDefaultMessages } from '../types';
 
-export const messages: TValidationMessages = {
+export const messages: TDefaultMessages = {
   type: 'invalid data type',
   required: 'required field',
   length: (length: number) => `field length must be ${length} characters`,
   short: (length: number) => `minimum field length ${length} characters`,
   long: (length: number) => `maximum field length ${length} characters`,
+  arrayLength: (length: number) => `field length must be ${length} characters`,
+  arrayShort: (length: number) => `minimum field length ${length} characters`,
+  arrayLong: (length: number) => `maximum field length ${length} characters`,
   small: (min: number) => `minimum value is greater than or equal to ${min}`,
   large: (max: number) => `the maximum value is less than or equal to ${max}`,
   format: 'field is invalid',
