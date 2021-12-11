0218591c5ea3959e32c5986bdc8cf6537c5ef929
Garik <garikfreedomyan@gmail.com>
2021-12-11 21:33:35 +0300

    finished number schema

--- src/schemas/number.ts
+++ src/schemas/number.ts
@@ -2,15 +2,26 @@ import { messages } from '../constants';
 import { TBaseOptions, BaseSchema } from './base';
 
 export type TNumberOptions = TBaseOptions & {
-  min?: { value: number; message?: string };
-  max?: { value: number; message?: string };
-  match?: { value: RegExp; message?: string };
-  equalField?: { name: string; message?: string };
+  min?: { value: number; message: string };
+  max?: { value: number; message: string };
 };
 
-class NumberSchema extends BaseSchema<TNumberOptions> {
+type TValidationResult = {
+  valid: boolean;
+  value: any;
+  error: string;
+};
+
+export class NumberSchema extends BaseSchema<TNumberOptions> {
   constructor(message?: string) {
-    super('number', message);
+    super('number', message ?? messages.number);
+  }
+
+  strict(message?: string) {
+    this.schema.type.strict = true;
+    this.schema.type.message = message ?? messages.number;
+
+    return this;
   }
 
   min(value: number, message?: string) {
@@ -29,20 +40,95 @@ class NumberSchema extends BaseSchema<TNumberOptions> {
     return this;
   }
 
-  match(regexp: RegExp, message?: string) {
-    this.schema.match = {
-      value: regexp,
-      message: message ?? messages.format,
+  validate(value: any): TValidationResult {
+    const result = {
+      valid: true,
+      value,
+      error: '',
     };
-    return this;
+
+    if (this.schema.required?.value && !this.validateRequired(value)) {
+      result.valid = false;
+      result.error = this.schema.required.message;
+      return result;
+    }
+
+    if (value !== undefined && value !== null && value !== '') {
+      if (this.validateType(value)) {
+        result.value = Number(value);
+      } else {
+        result.valid = false;
+        result.error = this.schema.type.message;
+        return result;
+      }
+    }
+
+    for (const option in this.schema) {
+      switch (option) {
+        case 'type':
+        case 'requred':
+          break;
+
+        case 'min':
+          if (this.validateMin(result.value, this.schema.min!.value)) break;
+          result.valid = false;
+          result.value = value;
+          result.error = this.schema.min!.message;
+          return result;
+
+        case 'max':
+          if (this.validateMax(result.value, this.schema.max!.value)) break;
+          result.valid = false;
+          result.value = value;
+          result.error = this.schema.max!.message;
+          return result;
+
+        default:
+          break;
+      }
+    }
+
+    return result;
   }
 
-  equalsTo(fieldName: string, message?: string) {
-    this.schema.equalField = {
-      name: fieldName,
-      message: message ?? messages.unequal(fieldName),
-    };
-    return this;
+  isValid(value: any): boolean {
+    return this.validate(value).valid;
+  }
+
+  cast(value: any): number {
+    const valid = this.validateType(value);
+
+    if (valid) {
+      return Number(value);
+    }
+
+    throw new TypeError(messages.number);
+  }
+
+  protected validateRequired(value: any): boolean {
+    if (value !== undefined && value !== null && value !== '') return true;
+
+    return false;
+  }
+
+  private validateType(value: any): boolean {
+    if (typeof value === 'number' && !Number.isNaN(value)) return true;
+    if (value instanceof Number && !Number.isNaN(value)) return true;
+    if (!this.schema.type.strict && typeof value === 'string' && value.trim() && !Number.isNaN(+value)) return true;
+
+    return false;
+  }
+
+  private validateMin(value: number, min: number): boolean {
+    if (value >= min) return true;
+
+    return false;
+  }
+
+  private validateMax(value: number, max: number): boolean {
+    if (value <= max) return true;
+
+    return false;
   }
 }
 
