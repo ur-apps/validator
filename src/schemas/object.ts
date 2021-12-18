d4f4ccc148e68348a4cef1032ee8521d2834ec6e
Garik <garikfreedomyan@gmail.com>
2021-12-18 22:39:04 +0300

    finished object schema

--- src/schemas/object.ts
+++ src/schemas/object.ts
@@ -1,23 +1,103 @@
 import { TBaseOptions, BaseSchema } from './base';
-import type { TArrayOptions, TBooleanOptions, TNumberOptions, TStringOptions } from 'schemas';
+import { clone, isObject } from '../utils';
+import { messages } from '../constants';
+import type { ArraySchema, BooleanSchema, NumberSchema, StringSchema } from '../schemas';
 
 export type TObjectOptions = TBaseOptions & {
   entries?: TObjectEntries;
 };
 
-type TObjectEntries = Record<
-  string,
-  TArrayOptions | TBooleanOptions | TNumberOptions | TObjectOptions | TStringOptions
->;
+type TObjectEntries = Record<string, ArraySchema | BooleanSchema | NumberSchema | StringSchema | ObjectSchema>;
 
-class ObjectSchema extends BaseSchema<TObjectOptions> {
+type TError =
+  | string
+  | {
+      [key: string]: TError;
+    };
+
+type TValidationResult = {
+  valid: boolean;
+  value: any;
+  error: TError;
+};
+
+export class ObjectSchema extends BaseSchema<TObjectOptions> {
   constructor(message?: string) {
-    super('object', message);
+    super('object', message ?? messages.object);
   }
 
   entries(entries: TObjectEntries) {
     this.schema.entries = entries;
     return this;
+  }
+
+  validate(value: Record<string, any>) {
+    const result: TValidationResult = {
+      valid: true,
+      value: value,
+      error: '',
+    };
+
+    if (this.schema.required?.value && !this.validateRequired(value)) {
+      result.valid = false;
+      result.error = this.schema.required.message;
+      return result;
+    }
+
+    if (value !== undefined && value !== null) {
+      if (!this.validateType(value)) {
+        result.valid = false;
+        result.error = this.schema.type.message;
+        return result;
+      }
+
+      if (this.schema.entries) {
+        result.value = clone(value);
+
+        for (const key in this.schema.entries) {
+          const entryResult = this.schema.entries[key].validate(result.value[key]);
+
+          if (entryResult.valid) {
+            result.value[key] = entryResult.value;
+          } else {
+            if (typeof result.error === 'string') result.error = {};
+            result.valid = false;
+            result.error[key] = entryResult.error;
+          }
+        }
+      }
+    }
+
+    if (!result.valid) result.value = value;
+    return result;
+  }
+
+  isValid(value: Record<string, any>) {
+    return this.validate(value).valid;
+  }
+
+  cast(value: Record<string, any>) {
+    if (isObject(value)) {
+      const validation = this.validate(value);
+
+      if (validation.valid) {
+        return validation.value;
+      }
+
+      throw TypeError(
+        typeof validation.error === 'string' ? validation.error : JSON.stringify(validation.error, undefined, 2)
+      );
+    }
+
+    throw TypeError(messages.object);
+  }
+
+  private validateType(value: any): boolean {
+    return isObject(value);
+  }
+
+  private validateRequired(value: any): boolean {
+    return value !== undefined && value !== null;
   }
 }
 
