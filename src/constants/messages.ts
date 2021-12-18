c998ca17c82984acef17aa6e7bef9b849107c044
Garik <garikfreedomyan@gmail.com>
2021-12-18 23:46:59 +0300

    added new messages and changed old

--- src/constants/messages.ts
+++ src/constants/messages.ts
@@ -2,6 +2,11 @@ import type { TDefaultMessages } from '../types';
 
 export const messages: TDefaultMessages = {
   type: 'invalid data type',
+  string: 'expected string value',
+  number: 'expected number value',
+  boolean: 'expected boolean value',
+  object: 'expected object',
+  array: 'expected array',
   required: 'required field',
   length: (length: number) => `field length must be ${length} characters`,
   short: (length: number) => `minimum field length ${length} characters`,
@@ -11,6 +16,8 @@ export const messages: TDefaultMessages = {
   arrayLong: (length: number) => `maximum field length ${length} characters`,
   small: (min: number) => `minimum value is greater than or equal to ${min}`,
   large: (max: number) => `the maximum value is less than or equal to ${max}`,
-  format: 'field is invalid',
+  format: 'invalid data format',
   unequal: (relField: string) => `field must match ${relField} field`,
+  true: 'value must be "true"',
+  false: 'value must be "false"',
 };
